cd1a2fdb76256c0b048d1d37e9515a6a76ac0fbf
Himanshu Raj <himanshuraj1209@gmail.com>
2023-07-30 02:42:28 +0530

    [UPD] my-app is uploaded

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import Video from './components/Video/Video';
+import './App.css';
+import {videos,items} from './data.js'
+import Border from './components/Video/Border/Border';
+import List from './components/List/List';
+export default function App(params) { 
+    return <>
+                
+                {/* <h1>{params.des}</h1>
+                <h2 style={{textAlign:'center'}}>User's Detials</h2>
+                <div className="container">
+                    {videos.map(video=>
+                        <Border>
+                            <Video className="Video1" key={video.id} {...video} width="400px"></Video>
+                        </Border>
+                    )}
+                    
+                </div> */}
+                {/* <List layout="numbered" items={items}></List> */}
+                <List layout="alpha" items={items}></List>
+            </>
+} 
---
+++ my-app/src/data.js
@@ -0,0 +1,27 @@
+export let videos = [
+    {
+        id:1,
+        title:"React JS tutorial",
+        views:"10k",
+        time:"3 year ago",
+        channel:"Coder Dost",
+        publish:false,
+    },
+    {
+        id:2,
+        title:"NOde JS tutorial",
+        views:"10k",
+        time:"1 year ago",
+        channel:"Coder Enemy",
+        publish:false,
+    },
+    {
+        id:3,
+        title:"Next JS tutorial",
+        views:"100k",
+        time:"2 year ago",
+        channel:"Coder Roat",
+        publish:false,
+    },
+] 
+export const items = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
